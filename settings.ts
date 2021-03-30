17a15667395eeaf4dcc6600e5713ec988f14be42
CodeXRed <75862826+CodeXRed@users.noreply.github.com>
2021-03-30 08:03:00 -0400

    Create settings.ts

---
+++ settings.ts
@@ -0,0 +1,30 @@
+export const logEvents = true;
+export const levelupEvents = true;
+
+export const giveWelcome = true;
+export const giveGoodbye = true;
+
+export const channels = {
+  announcements: '825749952977305644',
+  rules: '826167194261913620',
+  modlogs: '826171833660735540',
+  suggestions: '826172926234853427',
+  reports: '826172978106204160',
+  generalchat: '826172441792741397',
+  generalimages: '826172453553569802',
+  generalcommands: '826172467755221043',
+  information: '826167215044165692',
+  welcome: '',
+  goodbye: ''
+};
+
+export const roles = {
+  founder: '826163403491115068',
+  admin: '826163435854757949',
+  mod: '826163467496849438',
+  tmod: '826163491123363902',
+  verified: '826163521422884874',
+  muted: '826163561888743465'
+};
+
+export const disallowedMentions = [];
